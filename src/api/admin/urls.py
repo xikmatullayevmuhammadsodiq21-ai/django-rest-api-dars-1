493b014672e76957f9a350650a907d4895b5a1e7
Muhammadsodiq <xikmatullayevmuhammadsodiq21@gmail.com>
2026-03-31 19:04:49 +0500

    jashgiorg

--- src/api/admin/urls.py
+++ src/api/admin/urls.py
@@ -1,9 +1,14 @@
 
 from django.urls import path
 from api.admin.views.users_list import UserListApiView
-
+from api.admin.views.book_list import BookListApiView
+from api.admin.views.author_list import AuthorListApiView
+from api.admin.views.my_list import MyListApiView
 
 urlpatterns = [
     path("user/list/", UserListApiView.as_view()),
+    path("book/list/", BookListApiView.as_view()),
+    path("author/list/", AuthorListApiView.as_view()),
+    path("my/list/", MyListApiView.as_view()),
 ]
 
